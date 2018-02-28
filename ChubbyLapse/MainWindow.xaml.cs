9be9df06d639783e6d575163ebc93a7d5552b0b3
MightyChubz <theradcast16@gmail.com>
2018-02-28 00:59:24 -0500

    Implemented the interval slider.

--- ChubbyLapse/MainWindow.xaml.cs
+++ ChubbyLapse/MainWindow.xaml.cs
@@ -1,28 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChubbyLapse
 {
+    /// <inheritdoc cref="MainWindow" />
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void IntervalSelectorValueChanged(object sender, RoutedPropertyChangedEventArgs<double> eventArgs)
+        {
+            if (IntervalUpdateLabel == null) return;
+            IntervalSelector.Value = Convert.ToDouble(IntervalSelector.Value.ToString("F1"));
+            IntervalUpdateLabel.Content = IntervalSelector.Value;
         }
     }
 }
